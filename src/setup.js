556d6ab0f3f560c291f5f73b7929389548b0c67b
Maxim Kantsura <maxim@smoothr.de>
2023-06-14 00:09:03 +0300

    test

--- src/setup.js
+++ src/setup.js
@@ -5,12 +5,12 @@ const { User } = require('./models/user');
 // const { Token } = require('./models/token');
 // const { Expense } = require('./models/expenses');
 
-// User.sync({ force: true });
+User.sync({ force: true });
 // Token.sync({ force: true });
 // Expense.sync({ force: true });
 
-User.create({
-  email: "tiribofd560@akoption.com",
-  name:"maxim2310",
-  password:"Test1234",
-})
+// User.create({
+//   email: "tiribofd560@akoption.com",
+//   name:"maxim2310",
+//   password:"Test1234",
+// })
